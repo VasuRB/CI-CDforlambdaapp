a665ed6051f107131b5a9291657fe2c2fb733e62
Rambabu Vasupilli <vasu@ncplinc.com>
2019-11-29 00:20:29 -0500

    Updated Demo test

--- lambda_function.py
+++ lambda_function.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello this is from AWS SAM Lambda based Application !!')
+        'body': json.dumps('Hello this is from AWS SAM Lambda based Application !! Pre Demo Test')
     }
