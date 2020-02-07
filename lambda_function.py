5a102df89d11a8a3cd3af211ccc2b184afd97fb9
Rambabu Vasupilli <vasu@ncplinc.com>
2020-02-07 10:29:21 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello this is from AWS SAM Lambda based Application !! Demo Test by Rambabu Vasupilli')
+        'body': json.dumps('Hello this is from AWS SAM Lambda based Application !! Demo Test by Rambabu Vasupilli on 7th Feb 2020')
     }
